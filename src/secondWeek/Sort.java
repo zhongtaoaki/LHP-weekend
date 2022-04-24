42ff0e5ba8fbaa67e4bf889d4d489249672a8cd7
zhongtaoaki <zhongtaodylan@163.com>
2022-04-24 16:44:38 +0900

    冒泡排序

---
+++ src/secondWeek/Sort.java
@@ -0,0 +1,18 @@
+package secondWeek;
+
+public class Sort {
+    
+    static void sort(int[] array){
+
+        //TODO
+
+        for (int i : array) {
+            System.out.println(i);
+        } 
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2,5,1,4,3};
+        sort(array);
+    }
+}
