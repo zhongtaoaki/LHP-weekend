705ec6343cc34ccc551003e903ccc41f6ffaffef
zhongtaoaki <zhongtaodylan@163.com>
2022-05-15 16:08:48 +0900

    StringDemo

--- src/forthWeek/StringDemo.java
+++ src/forthWeek/StringDemo.java
@@ -1,29 +1,40 @@
 package forthWeek;
 
-public class StringDemo extends Object{
-    
-    public static void main(String[] args) {
-        
-        Fraction f1 = new Fraction(1, 3);
+public class StringDemo extends Object {
 
-        System.out.println(f1);
+    public static void main(String[] args) {
 
         Student s1 = new Student(1, "satou", 90);
         System.out.println(s1);
-        // 学号：1， 姓名：satou，成绩：90
-    }
 
+        Student[] students = new Student[5];
+        //90 ,92, 90, 60, 60
+        for (Student student : students) {
+         
+        }
+    }
 }
 
-class Student{
-    
+class Student {
+
     private int number;
     private String name;
     private int grade;
 
-    public Student(int number, String name,int grade){
+    public Student(int number, String name, int grade) {
         this.number = number;
         this.name = name;
         this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "学号：" + this.number + "， 姓名：" + this.name + "，成绩：" + this.grade;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
     }
 }
