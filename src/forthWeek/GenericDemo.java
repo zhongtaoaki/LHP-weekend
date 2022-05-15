0e5de3aceeb6c9c7ceeb88637675ac853c562bae
zhongtaoaki <zhongtaodylan@163.com>
2022-05-15 19:00:53 +0900

    范型

---
+++ src/forthWeek/GenericDemo.java
@@ -0,0 +1,32 @@
+package forthWeek;
+
+public class GenericDemo {
+
+    public static void main(String[] args) {
+
+        // Stack<String> stack = new Stack();
+        // stack.push("1");
+        // stack.push("20");
+        // stack.push(new Fraction(1, 3).toString());
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+    }
+
+}
+
+class Stack<T> {
+
+    // T[] array = new T[4];
+    // int index = 0;
+
+    // void push(T i) {
+    //     array[index] = i;
+    //     index++;
+    // }
+
+    // T pop() {
+    //     index--;
+    //     T i = array[index];
+    //     return i;
+    // }
+}
