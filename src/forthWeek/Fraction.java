c2eca2a993f13d04be928d2695a8a048ead86cf2
zhongtaoaki <zhongtaodylan@163.com>
2022-05-15 19:05:14 +0900

    update

--- src/forthWeek/Fraction.java
+++ src/forthWeek/Fraction.java
@@ -12,17 +12,33 @@ public class Fraction {
         }else{
             System.out.println("分母不能为0");
         }
-        
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        
+        //强制数据类型转换的语法
+        Fraction f = (Fraction)obj;
+
+        // boolean flag1 = this.numerator == f.numerator;
+        // boolean flag2 = this.denominator == f.denominator;
+        return (this.numerator == f.numerator) && (this.denominator == f.denominator);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Fraction(this.numerator, this.denominator);
+    }
+    
 
     public Fraction multiple(Fraction f) {
         //return new Fraction(f.numerator * this.numerator, f.denominator * this.denominator);
         int a = f.numerator * this.numerator;
         int b = f.denominator * this.denominator;
         return new Fraction(a, b);
-    
     }
 
+    @Override
     public String toString() {
         return numerator + "/" + denominator;
     }
