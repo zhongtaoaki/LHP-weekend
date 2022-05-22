3960b9b2db2bf819bb9aefb32553867f1bc212e8
zhongtaoaki <zhongtaodylan@163.com>
2022-05-22 15:49:11 +0900

    arrayList+scanner

--- src/fifthWeek/ArrayListDemo.java
+++ src/fifthWeek/ArrayListDemo.java
@@ -1,34 +1,36 @@
 package fifthWeek;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
+
+        ArrayList<Country> arrayList = new ArrayList<>();
+        // arrayList.add(new Country("China", "Beijing"));
+        // arrayList.add(new Country("Japan", "Tokyo"));
+        // arrayList.add(new Country("USA", "WDC"));
+        // arrayList.add(new Country("UK", "London"));
+
         
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("a");
-        arrayList.add("b");
-        arrayList.add("c");
-        arrayList.add("d");
-        arrayList.add("e");
-        arrayList.add("f");
-        arrayList.add(3,"i");
 
-        // for (String string : arrayList) {
-        //     System.out.println(string);
-        // }
+        Scanner scanner = new Scanner(System.in);
 
-        arrayList.set(0, "z");
-        arrayList.remove(3);
-        System.out.println(arrayList.size());
+        while (true) {
+            System.out.println("请输入国家（如果结束请输入end）");
+            String country = scanner.nextLine();
 
-        for (int i = 0; i < arrayList.size(); i++) {
-             System.out.println(arrayList.get(i));
+            if (country.equals("end")) {
+                break;
+            }
+            System.out.println("请输入这个国家的首都");
+            String capital = scanner.nextLine();
+            arrayList.add(new Country(country, capital));
         }
 
-
-        
-
+        for (Country country : arrayList) {
+            System.out.println(country);
+        }
 
     }
 }
