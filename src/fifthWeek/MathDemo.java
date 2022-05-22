9bc823ffeec863c8f9d27bf9f80845ee091494c9
zhongtaoaki <zhongtaodylan@163.com>
2022-05-22 12:01:32 +0900

    Math类

---
+++ src/fifthWeek/MathDemo.java
@@ -0,0 +1,19 @@
+package fifthWeek;
+
+public class MathDemo {
+    
+    public static void main(String[] args) {
+        
+        int x = 10;
+        int y = 20;
+
+        System.out.println(Math.max(x, y));
+
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.floor(10.5));
+        System.out.println(Math.ceil(10.5));
+        System.out.println(Math.round(10.5));
+        System.out.println(Math.pow(4, 2));
+        
+    }
+}
