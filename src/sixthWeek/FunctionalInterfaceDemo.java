389abcb21db26216bf19fabba11c00d27020da91
zhongtaoaki <zhongtaodylan@163.com>
2022-05-29 16:13:56 +0900

    filter

--- src/sixthWeek/FunctionalInterfaceDemo.java
+++ src/sixthWeek/FunctionalInterfaceDemo.java
@@ -2,22 +2,25 @@ package sixthWeek;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class FunctionalInterfaceDemo {
     public static void main(String[] args) {
 
-        Consumer consumer = new Consumer<Integer>() {
-            public void accept(Integer t) {
-                System.out.println(t);
-            }
-        };
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 65, 76);
 
-        List<Integer> list = List.of(1,2,3,4,5,65,76);
-        
-        //list.forEach(consumer);
-        list.forEach((Integer t) -> {
-            System.out.println(t);
-        });
-        
+        // list.forEach(consumer);
+        // list.forEach(t -> System.out.println(t));
+        // list.forEach(System.out::println);
+
+        Consumer consumer;// 有一个参数，没有返回值
+        Function function;// 有一个参数，有返回值
+        Supplier supplier;// 没有参数，有返回值
+        Predicate predicate;// 有参数，有返回值，且返回值为Boolean
+
+        list.stream().filter(i -> i > 10).forEach(System.out::println);
+
     }
 }
